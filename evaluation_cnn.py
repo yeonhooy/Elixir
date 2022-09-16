02bf2c4035d20e02de4e91c0ea61a6afe557b182
yeonhooy <yyho1994@naver.com>
2022-09-16 14:21:21 +0900

    evaluation mod

--- evaluation_cnn.py
+++ evaluation_cnn.py
@@ -1121,11 +1121,13 @@ def wirteInference(testdatafile, inference, resultfile, outputType, sheetname, s
     # ANSWER sheet for test_input
     #realdatafilename = "dataset_new_avg/inf_realvalue/new_score_csv/" + resultFileName + ".xlsx"
     realdatafilename = "dataset_new_avg/inf_realvalue/evaluation/" + resultFileName + ".xlsx"
+    realdatafilename = "dataset_new_avg/inf_realvalue/dt_csv/" + resultFileName + ".xlsx"
+    
     print(realdatafilename)
     real_wb = load_workbook(realdatafilename, data_only=True)
     #real_sheet = real_wb["Sheet"]
-    real_sheet = real_wb["Sheet1"]
-    #real_sheet = real_wb[resultFileName]
+    #real_sheet = real_wb["Sheet1"]
+    real_sheet = real_wb[resultFileName]
     # input("stop")
 
     #superemeloadfullpath = "dataset_new_avg/result/datcenter/" + result_name
@@ -1468,7 +1470,8 @@ def main():
 
         # QUESTION Sheet
         #testdatasetw = pd.read_csv('dataset_new_avg/inf_realvalue/new_score_csv/' + resultFileName + '.csv')
-        testdatasetw = pd.read_csv('dataset_new_avg/inf_realvalue/evaluation/' + resultFileName + '.csv')
+        #testdatasetw = pd.read_csv('dataset_new_avg/inf_realvalue/evaluation/' + resultFileName + '.csv')
+        testdatasetw = pd.read_csv('dataset_new_avg/inf_realvalue/dt_csv/' + resultFileName + '.csv')
 
         # --> test input is for test
         test_input = testdatasetw[['switch_e', 'switch_c', 'host', 'connection', 'interval', 'link', 'hop']]
